3c44922d8edfee45f1a18301a034eee36fdcf5fb
Lakshay Gupta <lakshaygupta2511@gmail.com>
2024-11-09 14:13:55 +0530

    Changed schem.prisma and clerk webhook also done

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,7 +1,24 @@
 // /api/clerk/webhook
 
+import { db } from "@/server/db";
+
 export const POST = async (req: Request) => {
   const { data } = await req.json();
+  const emailAddress = data.email_address[0].email_address;
+  const firstName = data.firs_name;
+  const lastName = data.last_name;
+  const imageUrl = data.image_url;
+  const id = data.id;
+
+  await db.user.create({
+    data: {
+      id,
+      emailAddress,
+      firstName,
+      lastName,
+      imageUrl,
+    },
+  });
 
   return new Response("Ok", { status: 200 });
 };
