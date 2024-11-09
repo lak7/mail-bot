d9a127d9c8f3117993923648462e4dc1556908ff
Lakshay Gupta <lakshaygupta2511@gmail.com>
2024-11-09 14:20:50 +0530

    Fixed typo in clerk webhook

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -4,8 +4,8 @@ import { db } from "@/server/db";
 
 export const POST = async (req: Request) => {
   const { data } = await req.json();
-  const emailAddress = data.email_address[0].email_address;
-  const firstName = data.firs_name;
+  const emailAddress = data.email_addresses[0].email_address;
+  const firstName = data.first_name;
   const lastName = data.last_name;
   const imageUrl = data.image_url;
   const id = data.id;
